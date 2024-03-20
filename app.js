b8d3c195bc52522392a0ffadcc31a1368bb9657a
ariclenespedro <pedroariclenes@gmail.com>
2024-03-20 10:58:12 +0100

    Configurando token para rotas privadas

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
 
 
 /* Routes Private */
-app.get("/cliente/:clientId", async (req, res) => {
+app.get("/cliente/:clientId", checkToken, async (req, res) => {
   const id = req.params.clientId;
 
   try {
@@ -54,6 +54,25 @@ app.get("/cliente/:clientId", async (req, res) => {
     return res.status(500).json({ msg: "Erro do servidor ao buscar o cliente!" });
   }
 });
+
+function checkToken(req,res,next) {
+
+  const authHeader = req.headers['Authorization'];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({msg:'Acesso negado'});
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    jwt.verify(token, secret);
+    next();
+  } catch (error) {
+    res.status(400).json({msg:'Token Inválido!'});
+  }
+  
+}
 
 
 //Register user
