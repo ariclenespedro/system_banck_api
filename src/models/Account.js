f52b99be5807e0585f323ba1544d8642977cb40a
ariclenespedro <pedroariclenes@gmail.com>
2024-03-22 06:53:25 +0100

    Criação da Entidade Accounts

--- src/models/Account.js
+++ src/models/Account.js
@@ -1,2 +1,12 @@
 const mongoose = require('mongoose');
 
+const Account = mongoose.model('Account',{
+    n_account : Number,
+    type_account : String,
+    currency: String,
+    descryption : String,
+    balance: Number(15,2),
+    iban : String,
+    cliente_id: Object
+
+});
