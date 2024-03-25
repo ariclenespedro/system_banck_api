0420f35a1a64f20343d5306fc4dd303be7c1f0cc
ariclenespedro <pedroariclenes@gmail.com>
2024-03-25 22:23:59 +0100

    Validando os dados da transação por tranferencia

--- app.js
+++ app.js
@@ -213,8 +213,18 @@ app.post('/cliente/payments/references', async (req, res) => {
     const {n_reference,amount,clientID,entity_id} = req.body;
 
     // Verificar se os dados necessários foram fornecidos
-    if (!clientId || !amount) {
+    if (!clientID || !amount) {
       return res.status(400).json({ message: 'É necessário fornecer o ID do cliente e o valor da transação' });
+    }
+    if (!n_reference) {
+      return res.status(422).json({ msg: "O número de referência é obrigatório!" });
+    }
+    if (!entity_id) {
+      return res.status(422).json({ msg: "O ID da Entidade é obrigatório!" });
+    }
+
+    if(entity_id === 1140223){
+      return res.status(422).json({ msg: "O ID da Entidade desconhecida!" });
     }
   } catch (error) {
     
