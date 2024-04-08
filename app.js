115da2a18f2685a4dc7045313ea5c2fa42fe69c3
ariclenespedro <pedroariclenes@gmail.com>
2024-04-08 22:29:03 +0100

    Função buscr entidade criada com sucesso.

--- app.js
+++ app.js
@@ -25,9 +25,11 @@ db();
 
 //Routers
 const routes = require('./src/routes/router');
+app.use('/api', routes);
+
+// Routes for api payments: entitys, references and payments
 const routesApipayment = require('./src/api_payment/routes/routes');
 app.use('/apiPayments', routesApipayment);
-app.use('/api', routes);
 
 
 // Definindo uma rota Pública.
--- src/api_payment/routes/entitys.js
+++ src/api_payment/routes/entitys.js
@@ -5,7 +5,7 @@ const entityController = require('../controllers/entityController');
 const auth = require("../../middleware/auth");
 
 router.route('/entity/create').post(auth,(req,res) => entityController.create(req,res));
-/* router.get('/entitys/:id', entityController.getEntitytById); */
+router.route('/entitys/:id').get(auth, (req,res) => entityController.getEntitytById(req,res));
 /* router.put('/entitys/:id', entityController.updateEntity);
 router.delete('/entitys/:id', entityController.deleteEntity); */
 
